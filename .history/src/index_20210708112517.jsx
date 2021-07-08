93e9f453eb1a16f7e7cc8055b369101c1283ddd0
alexandreifto2@gmail.com <alexandreifto2@gmail.com>
2021-07-08 11:51:44 -0300

    first commit

---
+++ .history/src/index_20210708112517.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import render from 'react-dom'
+import {App} from './App'
+
+render(<h1>TEst</h1>, document.getElementById('root'))
---
+++ .history/src/index_20210708113020.jsx
@@ -0,0 +1,5 @@
+import render from 'react-dom'
+import React from 'react';
+import {App} from './App'
+
+render(<h1>Test</h1>, document.getElementById('root'))
